69a1b8cd10d30f7932782c0ce3f86261d09ec89e
Boo Sung Jun <boorooksus@gmail.com>
2022-07-10 14:43:54 +0900

    feat: Set up enviroment
    Set up Vite + React + TypeScript + PWA enviroment

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Vite + React + TypeScript + PWA</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ sw.js
@@ -0,0 +1,22 @@
+// // install event
+// self.addEventListener("install", (e) => {
+//   console.log("Service Worker is installed", e);
+// });
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] installed");
+});
+
+// activate event
+self.addEventListener("activate", (e) => {
+  console.log("[Service Worker] actived", e);
+});
+
+// // fetch event
+// self.addEventListener("fetch", (e) => {
+//   console.log("fetch event", evt);
+// });
+
+self.addEventListener("fetch", (e) => {
+  console.log("[Service Worker] fetched resource " + e.request.url);
+});
