8f479b9035d02103825c26f483d61d4d44fb4ae4
Boo Sung Jun <boorooksus@gmail.com>
2022-08-31 15:05:52 +0900

    [GYJB-154] 검색창에서 X버튼을 누르면 검색어 초기화 후 컨텐츠 불러오도록 설정
    - 검색창에서 X버튼 누르면 검색어만 지우지 않고, 컨텐츠도 새로 불러오도록 설정
    - 검색창의 submitHandler와 onResetHandler 함수에  useCallback 사용

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { getContents } from '../utils/content';
 import {
   useRecoilState,
@@ -30,19 +30,32 @@ const Header = () => {
     [term, setTerm],
   );
 
-  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    resetPageIdx();
+  const onSubmitHandler = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      resetPageIdx();
+      getContents(contentsSize, curCategoryId, term).then((res) => {
+        setContents([...res]);
+      });
+    },
+    [contentsSize, curCategoryId, term, setContents],
+  );
 
-    getContents(contentsSize, curCategoryId, term).then((res) => {
-      setContents([...res]);
-    });
-  };
+  const onResetHandelr = useCallback(
+    (event: React.InputHTMLAttributes<HTMLInputElement>) => {
+      setTerm('');
+      getContents(contentsSize, curCategoryId, '').then((res) => {
+        setContents([...res]);
+      });
+    },
+    [setTerm, setContents],
+  );
 
   return (
     <header className="flex justify-center py-2  z-30">
       <div className="fixed top-0 w-full lg:w-9/12 p-2 z-30">
         <div className="flex items-center">
+          {/* Sidebar on/off button */}
           <button
             className="px-4 text-slate-400 hover:text-slate-400 lg:hidden "
             aria-controls="sidebar"
@@ -62,9 +75,9 @@ const Header = () => {
           </button>
           {/* Search */}
           <div className="flex justify-center w-full ">
-            <form onSubmit={onSubmitHandler} className="flex w-full">
+            <form onSubmit={onSubmitHandler} className="flex w-full relative">
               <input
-                type="search"
+                type="text"
                 className="form-control w-full
               p-2 relative block px-3 py-1.5 text-base font-normal text-gray-700 bg-white  border border-solid border-gray-300 rounded focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                 placeholder="Search"
@@ -72,7 +85,7 @@ const Header = () => {
                 onChange={onChangeHandler}
                 value={term}
                 tabIndex={1}
-              />
+              ></input>
               <button
                 type="submit"
                 className="w-12 h-10 flex items-center justify-center"
@@ -94,6 +107,26 @@ const Header = () => {
                   ></path>
                 </svg>
               </button>
+              {term && (
+                <button
+                  className="absolute right-16 top-2.5"
+                  onClick={onResetHandelr}
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="gray"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+              )}
             </form>
           </div>
         </div>
