aa58e9c021ecf1f38576f9bb8ae179c7f49faef0
Boo Sung Jun <boorooksus@gmail.com>
2022-09-28 21:20:26 +0900

    [GYJB-215] 로그인 에러 체크

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,6 +11,8 @@ const Login = () => {
     const params = new URLSearchParams(window.location.search);
     const accessToken = params.get('accessToken');
     const refreshToken = params.get('refreshToken');
+
+    console.log('accessToken', accessToken);
     if (accessToken) {
       localStorage.setItem('accessToken', JSON.stringify(accessToken));
       setLoginSuccess(true);
