8ef223530e9356d28b9eab5b8e805bd5278fb339
Boo Sung Jun <boorooksus@gmail.com>
2022-10-10 15:51:27 +0900

    [GYJB-206] 링크 모달 수정 버튼 수정
    - hover시 bg 색상 변경

--- src/partials/modals/LinkModal.tsx
+++ src/partials/modals/LinkModal.tsx
@@ -197,7 +197,7 @@ const Modal = (props: IProps) => {
             onClick={editHandler}
             data-modal-toggle="defaultModal"
             type="button"
-            className="text-white bg-signiture focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+            className="text-white bg-signiture hover:bg-green-400 focus:outline-none focus:ring-0 font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
           >
             수정
           </button>
