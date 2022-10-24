577d0fe1aad41ab43dd155dc6aa04a4f95e93b98
Boo Sung Jun <boorooksus@gmail.com>
2022-10-24 19:56:32 +0900

    [GYJB-206] Landing page 디자인 수정
    - 애니메이션 딜레이 시간 추가
    - 화면 크기 별 이미지 크기 설정
    - 버튼 크기 조절

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import google_logo from '../assets/images/google_logo.png';
-import { GoogleLogin } from 'react-google-login';
-import axios, { AxiosResponse } from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { requestAccessToken } from '../utils/auth';
 import logoBase from '../assets/images/logo_base.png';
@@ -26,7 +24,7 @@ const LandingPage = () => {
       {/* Linklip logo */}
       <div className="flex justify-between mb-20">
         <div className="bg-white grow z-20"></div>
-        <div className="relative w-1/2">
+        <div className="relative w-2/3 sm:w-2/5">
           <img
             src={logoBase}
             className="grow-0"
@@ -35,9 +33,9 @@ const LandingPage = () => {
           ></img>
           <img
             src={logoPoint}
-            className="absolute bottom-1 w-4/5"
+            className="absolute bottom-1 w-4/5 "
             data-aos="slide-right"
-            data-aos-delay="600"
+            data-aos-delay="700"
             alt="링클립 로고 밑줄"
           ></img>
         </div>
@@ -48,11 +46,11 @@ const LandingPage = () => {
         {detailOpen && (
           <div className="max-w-3xl mx-auto">
             <div data-aos="fade-left">
-              <p className="text-lg text-gray-600">
+              <p className="text-xl text-gray-600">
                 어떻게 하면 다양한 형태의 순간적인 정보를 간편하게 저장하고
                 체계적으로 관리할 수 있을까?
               </p>
-              <p className="text-lg text-gray-600 mb-16">
+              <p className="text-xl text-gray-600 mb-16">
                 효율적인 정보 관리를 위한 최고의 수단, {` `}
                 <span className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-signiture to-teal-400">
                   링클립
@@ -60,35 +58,65 @@ const LandingPage = () => {
               </p>
             </div>
 
-            <div data-aos="fade-right">
-              <p className="text-3xl text-gray-800 mb-4">
+            <div>
+              <p
+                className="text-3xl text-gray-800 mb-4"
+                data-aos="fade-right"
+                data-aos-delay="100"
+              >
                 순간적인 정보 저장과 관리
               </p>
               <img
                 src={serviceImage}
-                className="w-2/5 ml-auto mr-auto"
+                className="w-2/3 sm:w-2/5 ml-auto mr-auto"
                 alt="서비스 이미지"
+                data-aos="fade-right"
+                data-aos-delay="200"
               ></img>
-              <p className="text-lg text-gray-600">
+              <p
+                className="text-lg text-gray-600"
+                data-aos="fade-right"
+                data-aos-delay="300"
+              >
                 이미지, 텍스트, 링크 정보들을 간편하게 저장할 수 있습니다.
               </p>
-              <p className="text-lg text-gray-600 mb-16">
-                저장한 정보들을 그룹화하고, 빠르게 찾아 볼 수 있어요.
+              <p
+                className="text-lg text-gray-600 mb-16"
+                data-aos="fade-right"
+                data-aos-delay="300"
+              >
+                저장한 정보들을 그룹화하고, 빠르게 찾아보세요.
               </p>
             </div>
 
-            <div data-aos="fade-left">
-              <p className="text-3xl text-gray-800 mb-4">모바일 앱 지원</p>
+            <div>
+              <p
+                className="text-3xl text-gray-800 mb-4"
+                data-aos="fade-left"
+                data-aos-delay="100"
+              >
+                모바일 앱 지원
+              </p>
               <img
                 src={pwaInstall}
-                className="w-1/5 ml-auto mr-auto"
+                className="w-2/3 sm:w-1/5 ml-auto mr-auto"
                 alt="PWA 설치 방법"
+                data-aos="fade-left"
+                data-aos-delay="200"
               ></img>
-              <p className="text-lg text-gray-600">
+              <p
+                className="text-lg text-gray-600"
+                data-aos="fade-left"
+                data-aos-delay="300"
+              >
                 PWA 기술을 이용하여, 스마트폰에 바로가기를 추가하면 앱이
                 설치됩니다.
               </p>
-              <p className="text-lg text-gray-600 mb-16">
+              <p
+                className="text-lg text-gray-600 mb-16"
+                data-aos="fade-left"
+                data-aos-delay="300"
+              >
                 일상 생활 속에서 순간적인 정보들을 저장해봐요.
               </p>
             </div>
@@ -97,14 +125,14 @@ const LandingPage = () => {
 
         <div>
           {!detailOpen && (
-            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
+            <div className="max-w-xs mx-auto">
               <button
                 onClick={() => {
                   setDetailOpen(!detailOpen);
                 }}
-                className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4"
+                className="btn text-white bg-gray-900 hover:bg-gray-800 w-auto sm:ml-4"
               >
-                더 알아보기
+                서비스 알아보기
               </button>
             </div>
           )}
